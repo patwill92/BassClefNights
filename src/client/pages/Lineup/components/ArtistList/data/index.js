334b36eeb1409648ce89365918be3e94d7308cc7
Pat Williams <patrick.rw92@gmail.com>
2018-02-08 17:36:48 -0500

    change artist order on lineup

--- src/client/pages/Lineup/components/ArtistList/data/index.js
+++ src/client/pages/Lineup/components/ArtistList/data/index.js
@@ -1,5 +1,20 @@
 export default [
     {
+        name: 'Ed Calle',
+        info: `2015 Latin Grammy Award Winner for Best Instrumental Album and 5X Grammy nominee - Dr. Ed Calle. Around the globe, Calle is a renowned musician, composer, orchestrator, scholar, professor, and leader. Throughout his legendary career, Calle has become one of the most recorded saxophonists in history appearing on more than 1,200 albums, almost 9,000 singles, and countless television and movie soundtracks with artists ranging from Frank Sinatra and Celia Cruz to Sylvester Stallone and Andy Garcia.`,
+        image: 'EdCalle.jpeg'
+    },
+    {
+        name: 'Bossa Nova',
+        info: `Bossa Nova is a musical sub-genre derived from samba and with strong American jazz influences. Initially, the term Bossa Nova referred to a way of singing and playing samba but gradually became one of the most important movements in what is known in Brazil as MPB.`,
+        image: 'bossanova.jpg'
+    },
+    {
+        name: 'Lary Barilleau & The Latin Jazz Collective',
+        info: `The Latin Jazz Collective is a new project conceived by Lary Barilleau, a Seattle based percussionist, composer and award winning educator. Carmen's Mambo features the original music of Grammy nominated musician Lary Barilleau, Grammy winning arranger and trombonist Doug Beavers and multi-instrumentalist Jay Thomas. The unique mixture of jazz melodies and harmonies along with Afro-Caribbean rhythms reflects the classic sound of Latin jazz.`,
+        image: 'LaryBarilleau.jpg'
+    },
+    {
         name: 'TITO ALVIZURIS & THE LATIN JAZZ TRIO',
         info: `Son of two concert pianists , Tito has studied music since childhood. Tito is an
                                 exceptional trumpeter, but can easily turn his musical talents to the drums.`,
@@ -9,21 +24,6 @@ export default [
         name: 'Tony Pastrana',
         info: `Percussionist Tony Pastrana, the ultimate bandleader, brings us a host of smooth, high energy Latin grooves as Orchestra Pastrana plays Joey Pastrana's greatest hits.`,
         image: 'TonyP.jpg'
-    },
-    {
-        name: 'Bossa Nova',
-        info: `Bossa Nova is a musical sub-genre derived from samba and with strong American jazz influences. Initially, the term Bossa Nova referred to a way of singing and playing samba but gradually became one of the most important movements in what is known in Brazil as MPB.`,
-        image: 'bossanova.jpg'
-    },
-    {
-        name: 'Ed Calle',
-        info: `2015 Latin Grammy Award Winner for Best Instrumental Album and 5X Grammy nominee - Dr. Ed Calle. Around the globe, Calle is a renowned musician, composer, orchestrator, scholar, professor, and leader. Throughout his legendary career, Calle has become one of the most recorded saxophonists in history appearing on more than 1,200 albums, almost 9,000 singles, and countless television and movie soundtracks with artists ranging from Frank Sinatra and Celia Cruz to Sylvester Stallone and Andy Garcia.`,
-        image: 'EdCalle.jpeg'
-    },
-    {
-        name: 'Lary Barilleau & The Latin Jazz Collective',
-        info: `The Latin Jazz Collective is a new project conceived by Lary Barilleau, a Seattle based percussionist, composer and award winning educator. Carmen's Mambo features the original music of Grammy nominated musician Lary Barilleau, Grammy winning arranger and trombonist Doug Beavers and multi-instrumentalist Jay Thomas. The unique mixture of jazz melodies and harmonies along with Afro-Caribbean rhythms reflects the classic sound of Latin jazz.`,
-        image: 'LaryBarilleau.jpg'
     },
     {
         name: 'Patricia Mesa',
